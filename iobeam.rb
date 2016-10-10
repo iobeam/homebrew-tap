1d8f936f6725c12f74bd7a113148a0336e5bf046
Rob Kiefer <rob.kiefer@gmail.com>
2016-10-10 16:36:36 -0400

    Update to v0.9.1

--- iobeam.rb
+++ iobeam.rb
@@ -1,9 +1,9 @@
 class Iobeam < Formula
   desc "The iobeam command-line tool"
   homepage "https://github.com/iobeam/iobeam"
-  url "https://assets.iobeam.com/releases/iobeam-0.9.0.tar.gz"
-  version "0.9.0"
-  sha256 "409160b432a197b1e2380cc3e6e62e65caa07c424e15dde4474ead8cafe0aa3d"
+  url "https://assets.iobeam.com/releases/iobeam-0.9.1.tar.gz"
+  version "0.9.1"
+  sha256 "8aee6ce620d42e3f02204fc0d56771ec81f79acd6a6aa443f4ef4118b3bad5c3"
 
   def install
     bin.install "iobeam"
